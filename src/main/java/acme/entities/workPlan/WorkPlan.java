2a2736a90e18d47a1437dcc6919fd34f5d1a09f6
Alejandro Morales Garcia <alemorgar4@alum.us.es>
2021-05-02 12:40:01 +0200

    Función obtener fechas realizado

--- src/main/java/acme/entities/workPlan/WorkPlan.java
+++ src/main/java/acme/entities/workPlan/WorkPlan.java
@@ -1,7 +1,10 @@
 package acme.entities.workPlan;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
@@ -46,13 +49,18 @@ public class WorkPlan extends DomainEntity  {
 	
 	@Transient
 	public Double getWorkload() {
-		Double res = 0.;
-		
+		Integer a = 0;
 		for(final Task task: this.tasks) {
-			res += task.getWorkload();
+			a += task.getMinutes();
 		}
-		
-		return res;
+		Double b = a/60.0;
+		Integer parteEntera = b.intValue();
+		Integer parteFracional = (int) ((b - parteEntera)*100);
+		if(parteFracional>=60) {
+			parteEntera += 1;
+			parteFracional -= 60;
+		}
+		return parteEntera+parteFracional*0.01;
 	}
 	
 	public Boolean isValid() {
@@ -78,5 +86,35 @@ public class WorkPlan extends DomainEntity  {
 		}
 		
 		return res;
+	}
+	
+	public void setStartExecutionPeriodAndEndExecutionPeriodRand() {
+		List<Task> tasks1 = this.tasks.stream().collect(Collectors.toList());
+		Date start1 = tasks1.get(0).getStartExecutionPeriod();
+		Date finish1 = tasks1.get(0).getEndExecutionPeriod();
+		for(Task task:tasks1) {
+			if(task.getStartExecutionPeriod().before(start1)) {
+				start1=task.getStartExecutionPeriod();
+			}
+			else if(task.getEndExecutionPeriod().after(finish1)) {
+				finish1 = task.getEndExecutionPeriod();
+			}
+		}
+		Calendar c = Calendar.getInstance();
+		c.setTime(start1);
+		c.set(Calendar.HOUR_OF_DAY, 8);
+		c.set(Calendar.MINUTE, 00);
+		c.add(Calendar.DATE, -1);
+		start1 = c.getTime();
+		
+		Calendar c2 = Calendar.getInstance();
+		c2.setTime(finish1);
+		c2.set(Calendar.HOUR_OF_DAY, 17);
+		c2.set(Calendar.MINUTE, 00);
+		c2.add(Calendar.DATE, 1);
+		finish1 = c2.getTime();
+
+		this.startExecutionPeriod = start1;
+		this.endExecutionPeriod = finish1;
 	}
 }
