1454909c4b0314569bb4fbac4acb0520c0a75f6f
Usuario <Usuario@DESKTOP-H9Q10E5>
2021-05-15 18:44:54 +0200

    PRUEBAS PARA FEATURE ADMINISTRATOR CONFIGURATION
    Pruebas para la feature: un usuario administrador puede ver y modificar
    los parámetros de configuración.

---
+++ src/test/java/acme/testing/administrator/configuration/AdministratorConfigurationUpdateTest.java
@@ -0,0 +1,67 @@
+package acme.testing.administrator.configuration;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AdministratorConfigurationUpdateTest extends AcmePlannerTest {
+	
+	/*	Feature: un usuario administrador puede modificar los parámetros de configuración de la
+	 * 	aplicación (palabras spam, umbral de spam).
+	 * 	Caso positivo.
+	*/
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/configuration/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(40)	
+	public void updatePositive(final String spamWords, final String threshold) {		
+		super.signIn("administrator", "administrator");
+		
+		super.clickOnMenu("Administrator", "Show configuration");		
+				
+		super.clickOnReturnButton("Edit");
+		
+		super.fillInputBoxIn("spamWords", spamWords);
+		super.fillInputBoxIn("threshold", threshold);
+		super.clickOnSubmitButton("Update");
+
+		super.clickOnMenu("Administrator", "Show configuration");		
+
+		super.checkInputBoxHasValue("spamWords", spamWords.toLowerCase());
+		super.checkInputBoxHasValue("threshold", threshold);	
+		
+		super.signOut();
+	}
+	
+	
+	/*	Feature: un usuario administrador puede modificar los parámetros de configuración de la
+	 * 	aplicación (palabras spam, umbral de spam).
+	 * 	Caso negativo.
+	*/
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/configuration/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(40)	
+	public void updateNegative(final String spamWords, final String threshold) {		
+		super.signIn("administrator", "administrator");
+		
+		super.clickOnMenu("Administrator", "Show configuration");		
+				
+		super.clickOnReturnButton("Edit");
+	
+		super.fillInputBoxIn("spamWords", spamWords);
+		super.fillInputBoxIn("threshold", threshold);
+		super.clickOnSubmitButton("Update");
+		
+		super.checkErrorsExist("spamWords");
+		super.checkErrorsExist("threshold");
+
+		super.signOut();
+	}
+
+
+}
