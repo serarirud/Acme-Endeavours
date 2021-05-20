f026eecc9ac7b6c0e9f035f31e1f3d480e9fcfef
Alejandro Morales Garcia <alemorgar4@alum.us.es>
2021-05-20 13:33:17 +0200

    Revertir

--- src/main/java/acme/framework/testing/AbstractTest.java
+++ src/main/java/acme/framework/testing/AbstractTest.java
@@ -368,7 +368,9 @@ public abstract class AbstractTest {
 	protected void clickAndWait(final WebElement element) {
 		assert element != null;
 
-		this.navigate(() -> this.clickAndGo(element));
+		this.navigate(() -> {
+			this.clickAndGo(element);
+		});
 		this.longSleep();
 	}
 
