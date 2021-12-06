a96dac18f8645dbe906143de9d998f5c84691b7f
Sergio Arias Ruda <serarirud@alum.us.es>
2021-12-06 10:03:35 +0100

    Tests actualizados

--- src/test/java/acme/testing/administrator/dashboard/AdministratorDashboardShowTest.java
+++ src/test/java/acme/testing/administrator/dashboard/AdministratorDashboardShowTest.java
@@ -15,28 +15,28 @@ public class AdministratorDashboardShowTest extends AcmeEndeavoursTest {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/administrator/dashboard/show.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)	
-	public void showPositive(final String nPrivateTask, final String nPublicTask, final String nFinishedTask, final String nNotFinishedTask, 
-		final String averageTaskExecutionPeriods, final String deviationTaskExecutionPeriods, 
-		final String minimumTaskExecutionPeriods, final String maximumTaskExecutionPeriods, 
-		final String averageTaskWorkloads, final String deviationTaskWorkloads, 
-		final String minimumTaskWorkloads, final String maximumTaskWorkloads) {
+	public void showPositive(final String nPrivateDuties, final String nPublicDuties, final String nFinishedDuties, final String nNotFinishedDuties, 
+		final String averageDutiesExecutionPeriods, final String deviationDutiesExecutionPeriods, 
+		final String minimumDutiesExecutionPeriods, final String maximumDutiesExecutionPeriods, 
+		final String averageDutiesWorkloads, final String deviationDutiesWorkloads, 
+		final String minimumDutiesWorkloads, final String maximumDutiesWorkloads) {
 		
 		super.signIn("administrator", "administrator");
 		
 		super.clickOnMenu("Administrator", "Dashboard");
 		
-		super.checkErrorsAcmeMessageExist(2,1,1,nPrivateTask.toString());
-		super.checkErrorsAcmeMessageExist(2,1,2,nPublicTask.toString());
-		super.checkErrorsAcmeMessageExist(2,1,3,nFinishedTask.toString());
-		super.checkErrorsAcmeMessageExist(2,1,4,nNotFinishedTask.toString());
-		super.checkErrorsAcmeMessageExist(2,2,1,averageTaskExecutionPeriods.toString() + " Days");
-		super.checkErrorsAcmeMessageExist(2,2,2,deviationTaskExecutionPeriods.toString() + " Days");
-		super.checkErrorsAcmeMessageExist(2,2,3,minimumTaskExecutionPeriods.toString() + " Days");
-		super.checkErrorsAcmeMessageExist(2,2,4,maximumTaskExecutionPeriods.toString() + " Days");
-		super.checkErrorsAcmeMessageExist(2,3,1,averageTaskWorkloads.toString() + " {Hours,Minutes}");
-		super.checkErrorsAcmeMessageExist(2,3,2,deviationTaskWorkloads.toString() + " {Hours,Minutes}");
-		super.checkErrorsAcmeMessageExist(2,3,3,minimumTaskWorkloads.toString() + " {Hours,Minutes}");
-		super.checkErrorsAcmeMessageExist(2,3,4,maximumTaskWorkloads.toString() + " {Hours,Minutes}");
+		super.checkErrorsAcmeMessageExist(2,1,1,nPrivateDuties.toString());
+		super.checkErrorsAcmeMessageExist(2,1,2,nPublicDuties.toString());
+		super.checkErrorsAcmeMessageExist(2,1,3,nFinishedDuties.toString());
+		super.checkErrorsAcmeMessageExist(2,1,4,nNotFinishedDuties.toString());
+		super.checkErrorsAcmeMessageExist(2,2,1,averageDutiesExecutionPeriods.toString() + " Days");
+		super.checkErrorsAcmeMessageExist(2,2,2,deviationDutiesExecutionPeriods.toString() + " Days");
+		super.checkErrorsAcmeMessageExist(2,2,3,minimumDutiesExecutionPeriods.toString() + " Days");
+		super.checkErrorsAcmeMessageExist(2,2,4,maximumDutiesExecutionPeriods.toString() + " Days");
+		super.checkErrorsAcmeMessageExist(2,3,1,averageDutiesWorkloads.toString() + " {Hours,Minutes}");
+		super.checkErrorsAcmeMessageExist(2,3,2,deviationDutiesWorkloads.toString() + " {Hours,Minutes}");
+		super.checkErrorsAcmeMessageExist(2,3,3,minimumDutiesWorkloads.toString() + " {Hours,Minutes}");
+		super.checkErrorsAcmeMessageExist(2,3,4,maximumDutiesWorkloads.toString() + " {Hours,Minutes}");
 
 		super.signOut();
 	}
